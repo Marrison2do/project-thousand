a4fc6c50db5beec5a6ced6f543d2545e7c036b97
Marrison2do <alfonso.marrero98@gmail.com>
2023-06-19 14:40:31 -0300

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>
-    <Link to "/">  
-    Cliente
-    </Link>
-  </div>;
+  return (
+    <div>
+      <Link to="/">Clientes</Link>
+      <Link to="/companies">Companias</Link>
+      <Link to="/tasks">Tareas</Link>
+      <Link to="/invoices">Facturas</Link>
+      <Link to="/receipts">Recibos</Link>
+      <Link to="/checks">Cheques</Link>
+      <div>
+        <p>usuario</p>
+        <button>Cerrar sesion</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
